d794be1e51bed60a4988bfc4c8c0a4bb190c6fe2
diamond_dragon <diamond_dragon@tut.by>
2021-02-08 18:59:51 +0300

    Logging added

--- src/CodeReview.Evaluator/Services/EvaluationService.cs
+++ src/CodeReview.Evaluator/Services/EvaluationService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using GodelTech.CodeReview.Evaluator.Models;
+using Microsoft.Extensions.Logging;
 
 namespace GodelTech.CodeReview.Evaluator.Services
 {
@@ -10,15 +11,18 @@ namespace GodelTech.CodeReview.Evaluator.Services
         private readonly IFileService _fileService;
         private readonly IJsonSerializer _jsonSerializer;
         private readonly IDbRequestExecutorFactory _executorFactory;
+        private readonly ILogger<EvaluationService> _logger;
 
         public EvaluationService(
             IFileService fileService, 
             IJsonSerializer jsonSerializer,
-            IDbRequestExecutorFactory executorFactory)
+            IDbRequestExecutorFactory executorFactory,
+            ILogger<EvaluationService> logger)
         {
             _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
             _jsonSerializer = jsonSerializer ?? throw new ArgumentNullException(nameof(jsonSerializer));
             _executorFactory = executorFactory ?? throw new ArgumentNullException(nameof(executorFactory));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
         
         public async Task EvaluateAsync(EvaluationManifest manifest, string dbFilePath, string outputFilePath)
@@ -35,12 +39,17 @@ namespace GodelTech.CodeReview.Evaluator.Services
             
             foreach (var (requestName, dbRequestManifest) in manifest.Requests)
             {
+                _logger.LogInformation("Starting evaluation of \"{request}\" request...",  requestName);
+                
                 var queryResult = await executor.ExecuteAsync(requestName);
 
                 if (dbRequestManifest.AddToOutput)
+                    // TODO: Evaluate quality gate
                     result.Add(requestName, queryResult);
+
+                _logger.LogInformation("Evaluation completed", requestName);
             }
-            
+
             var json = _jsonSerializer.Serialize(result);
 
             await _fileService.WriteAllTextAsync(outputFilePath, json);
