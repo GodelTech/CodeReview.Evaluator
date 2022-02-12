d0b071328730208c2dfeae4761d27804c14b8b48
diamond_dragon <diamond_dragon@tut.by>
2022-02-12 20:37:06 +0300

    Resource infrastructure added

---
+++ test/CodeReview.Evaluator.IntegrationTests/Actions/RunEvaluate.cs
@@ -0,0 +1,27 @@
+﻿using CodeReview.Evaluator.IntegrationTests.Utils;
+using StoryLine.Contracts;
+
+namespace CodeReview.Evaluator.IntegrationTests.Actions
+{
+    public class RunEvaluate : IActionBuilder
+    {
+        private static readonly ResourceNameResolver ResourceNameResolver = new();
+        private static readonly string SqliteDatabaseResourceName = ResourceNameResolver.FindResourceInResourceFolder("issues.db");
+
+        private string _manifestResourceName;
+
+        public IActionBuilder WithManifestFromResources()
+        {
+            _manifestResourceName = ResourceNameResolver.FindResultResourceNameByMethod();
+
+            return this;
+        }
+
+        public IAction Build()
+        {
+            return new RunEvaluateAction(
+                SqliteDatabaseResourceName,
+                _manifestResourceName);
+        }
+    }
+}
---
+++ test/CodeReview.Evaluator.IntegrationTests/Utils/ResourceNameResolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CodeReview.Evaluator.IntegrationTests.Utils
+{
+    internal class ResourceNameResolver
+    {
+        private static readonly HashSet<string> AssemblyResourceNames = new(typeof(ResourceNameResolver).Assembly.GetManifestResourceNames(), StringComparer.InvariantCultureIgnoreCase);
+        private static readonly TestMethodAttributeBasedFilter AttributeBasedFilter = new();
+
+        // Order is important for performance reasons
+        private static readonly string[] Suffixes =
+        {
+            ".approved.",
+            ""
+        };
+
+        public string FindResourceInResourceFolder(string relativeName)
+        {
+            var expectedResourceName = "CodeReview.Evaluator.IntegrationTests.Resources" + relativeName;
+
+            return AssemblyResourceNames.TryGetValue(expectedResourceName, out var actualResourceName) ? 
+                actualResourceName : 
+                string.Empty;
+        }
+
+        public string FindResultResourceNameByMethod()
+        {
+            var trace = new StackTrace();
+
+            var frames = trace.GetFrames();
+
+            var frame = frames.FirstOrDefault(AttributeBasedFilter.IsMatch);
+            if (frame == null)
+                return string.Empty;
+
+            return GetResourceName(frame);
+        }
+
+        private static string GetResourceName(StackFrame frame)
+        {
+            var typeName = frame.GetType().FullName + "." + frame.GetMethod()?.Name;
+
+            foreach (var suffix in Suffixes)
+            {
+                var matchingResource = AssemblyResourceNames.FirstOrDefault(x => x.Equals(typeName + suffix, StringComparison.InvariantCultureIgnoreCase));
+
+                if (!string.IsNullOrEmpty(matchingResource))
+                    return matchingResource;
+            }
+
+            return string.Empty;
+        }
+    }
+}
