c73a82a532a23fa8046e687eeb2c4f942d9daf21
diamond_dragon <diamond_dragon@tut.by>
2022-02-12 22:05:02 +0300

    Tests are being added

--- test/CodeReview.Evaluator.IntegrationTests/Commands/Evaluate/EvaluateTests.cs
+++ test/CodeReview.Evaluator.IntegrationTests/Commands/Evaluate/EvaluateTests.cs
@@ -7,7 +7,18 @@ namespace CodeReview.Evaluator.IntegrationTests.Commands.Evaluate
     public class EvaluateTests
     {
         [Fact]
-        public void When_ManifestContainsScalarValue_Should_ReturnExpectedJson()
+        public void When_ScalarResult_Should_ReturnExpectedJson()
+        {
+            Scenario.New()
+                .When()
+                    .Performs<Actions.Evaluate>(x => x.WithManifestFromResources())
+                .Then()
+                    .Expects<EvaluationResult>(x => x.WithOutputFromResources())
+                .Run();
+        }
+
+        [Fact]
+        public void When_QueryResult_Should_ReturnExpectedJson()
         {
             Scenario.New()
                 .When()
