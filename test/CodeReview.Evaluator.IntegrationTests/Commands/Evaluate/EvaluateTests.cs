5c1620d931316d4a847ea841fb3413041bfce19e
diamond_dragon <diamond_dragon@tut.by>
2022-02-13 10:13:56 +0300

    Mixed queries test added

--- test/CodeReview.Evaluator.IntegrationTests/Commands/Evaluate/EvaluateTests.cs
+++ test/CodeReview.Evaluator.IntegrationTests/Commands/Evaluate/EvaluateTests.cs
@@ -7,6 +7,17 @@ namespace CodeReview.Evaluator.IntegrationTests.Commands.Evaluate
     public class EvaluateTests
     {
         [Fact]
+        public void When_AllQueryTypes_Should_ReturnExpectedJson()
+        {
+            Scenario.New()
+                .When()
+                .Performs<Actions.Evaluate>(x => x.WithManifestFromResources())
+                .Then()
+                .Expects<EvaluationResult>(x => x.WithOutputFromResources())
+                .Run();
+        }
+
+        [Fact]
         public void When_ScalarResult_Should_ReturnExpectedJson()
         {
             Scenario.New()
